f0e05594e1db12f51cd6595caadee0a536af7feb
Tomas Bjerre <tomas.bjerre85@gmail.com>
2021-11-20 12:41:00 +0100

    feat: npm-java-runner

--- src/index.ts
+++ src/index.ts
@@ -4,41 +4,40 @@ import path from 'path';
 import fs from 'fs';
 import child_process from 'child_process';
 
-function execute(command:string){
-    child_process.exec(command, function(error, stdout, stderr){
-        if (error) {
-            console.error(error)
-        }
-        if (stderr) {
-            console.error(stderr)
-        }
-        if (stdout) {
-            console.log(stdout)
-        }
-    });
-}
-
-function search(dir:string, regex:string):string[] {
-    let result:string[] = [];
-    for (let file in fs.readdirSync(dir)) {
-        const filePath = path.join(dir, file);
-        const stat = fs.statSync(filePath);
-        if (stat.isFile() && new RegExp(regex).test(filePath)) {
-            result.push(filePath);
-        } else if (stat.isDirectory()) {
-            result = result.concat(search(filePath, regex));
-        }
+function execute(command: string) {
+  child_process.exec(command, function (error, stdout, stderr) {
+    if (error) {
+      console.error(error);
     }
-    return result;
+    if (stderr) {
+      console.error(stderr);
+    }
+    if (stdout) {
+      console.log(stdout);
+    }
+  });
 }
 
+function search(dir: string, regex: string): string[] {
+  let result: string[] = [];
+  for (let file in fs.readdirSync(dir)) {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+    if (stat.isFile() && new RegExp(regex).test(filePath)) {
+      result.push(filePath);
+    } else if (stat.isDirectory()) {
+      result = result.concat(search(filePath, regex));
+    }
+  }
+  return result;
+}
 
 function resolveRunnableJar(regexp: string, path: string) {
-    const found = search(path,regexp);
-    if (found.length > 1) {
-        throw `Multiple matches of ${regexp} in ${path} ${JSON.stringify(found)}`;
-    }
-    return found[0];
+  const found = search(path, regexp);
+  if (found.length > 1) {
+    throw `Multiple matches of ${regexp} in ${path} ${JSON.stringify(found)}`;
+  }
+  return found[0];
 }
 
 export function run(userPath: string) {
@@ -54,6 +53,6 @@ export function run(userPath: string) {
   }
 
   const runnableJar = resolveRunnableJar(runnableJarRegexp, userPath);
-  
-  execute(`java -jar ${runnableJar}`)
+
+  execute(`java -jar ${runnableJar}`);
 }
